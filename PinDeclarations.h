21c9fe85c4b1a66746709d5e0a85234b98c97ff0
eilims <dbenusovich@gmail.com>
2017-11-01 18:41:12 -0700

    Added all pins declarations for Arduino Uno

--- PinDeclarations.h
+++ PinDeclarations.h
@@ -8,8 +8,10 @@
 //Import to allow arduino commands 
 #include <Arduino.h>
 
+
+//THIS PIN DECLARATION IS FOR ARDUINO UNO ONLY
+//CHANGE THIS ALL FOR ARDUINO NANO
 //List all pins used here
-//No declaring
 
 //Left Motors
 const int MOTOR_SPEED_LEFT = 10;
@@ -21,8 +23,43 @@ const int MOTOR_SPEED_RIGHT = 9;
 const int MOTOR_ENABLE_RIGHT_FRONT = 6;
 const int MOTOR_ENABLE_RIGHT_BACK = 5;
 
-//feelers
-const int LEFT_FEELER = 12;
-const int RIGHT_FEELER = 13;
+//Feelers
+//We could use logic gates to incorporate multiple feelers
+const int FEELER = 12;
+
+//Moisture Sensor
+//We combine Voltage_pin and motor_direction for the rack and pinion motor due to a lack of pins
+//We may combine the pins because the moisture sensor will only be on when we lower the motor
+//const int VOLTAGE_PIN = 4;
+const int MOISTURE_INPUT = 14;
+
+//Proximity Sensor
+const int ULTRASOUND_TRIGGER = 11;
+const int ULTRASOUND_ECHO = 3;
+
+//XBee
+const int XBEE_TX = 1;
+const int XBEE_RX = 0;
+
+//Water Sensor
+const int WATER_INPUT = 15;
+
+//Battery Voltage
+const int BATTERY_VOLTAGE = 16;
+
+//Temperature Sensor
+const int TEMPERATURE_INPUT = 17;
+
+//GPS
+const int GPS_RX = 2;
+const int GPS_TX = 17;
+
+//IMU
+const int IMU_SDA = 18;
+const int IMU_SCL = 19;
+
+//Rack and Pinion Motor
+const int MOTOR_MOISTURE_UP = 13;
+const int MOTOR_MOISTURE_DOWN_AND_MOISTURE_VOLTAGE = 4;
 
 #endif //H2ROVER_PINDECLARATIONS_H
