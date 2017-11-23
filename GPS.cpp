df40532d705c319e1e23642322b7777fe55632a8
eilims <dbenusovich@gmail.com>
2017-11-22 16:59:52 -0800

    inital commit

---
+++ GPS.cpp
@@ -0,0 +1,83 @@
+//
+// Created by: Jonathan Midolo
+// Created on: 11/19/2017
+// Last edited by: Jonathan Midolo
+// Last edited bn: 11/20/2017
+//
+// Edit notes:
+//		(11-19-2017) Created
+
+
+#include "GPS.h"
+
+// Constructor
+GPS::GPS() {
+
+}
+
+// Deconstructor
+GPS::~GPS() {
+
+}
+
+void GPS::initialize(Adafruit_GPS* GPS_obj1, SoftwareSerial* softSerial) {
+
+	// Setup onboard Arduino Nano LED for fix confirmation
+	pinMode(LED_BUILTIN,OUTPUT);
+	digitalWrite(LED_BUILTIN,LOW);
+
+	// Initialize GPS
+	// 9600 NMEA is the default baud rate for Adafruit MTK GPS
+	Serial.println("GPS");
+	Serial.println(GPS_obj1->hour);
+
+	GPS_obj1->begin(9600);
+
+
+	GPS_obj1->sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+	GPS_obj1->sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+	GPS_obj1->sendCommand(PGCMD_ANTENNA);
+	delay(1000);
+
+	while(GPS_obj1->fix != 1){
+		Serial.println("GPS not ready");
+		char letter = GPS_obj1->read();
+		// if (letter = '0'){
+		// 	Serial.println("Software Serial Error");
+		// } else {
+		// 	if (!GPS_obj1->fix) {
+		// 	Serial.println("GPS fix: ");
+		// 	Serial.println(GPS_obj1->fix);
+		// 	}
+
+			if (GPS_obj1->newNMEAreceived()) {
+		
+			if (!GPS_obj1->parse(GPS_obj1->lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+				return;  // we can fail to parse a sentence in which case we should just wait for another
+			}
+			Serial.println(GPS_obj1->newNMEAreceived());
+			delay(2000);
+		//}
+
+	}
+	Serial.println("GPS ready");
+}
+
+void GPS::gpsRead(Adafruit_GPS* GPS_obj1)
+{
+	Serial.println("GPS");
+	Serial.println((int)GPS_obj1);
+	// if a sentence is received, we can check the checksum, parse it...
+	if (GPS_obj1->newNMEAreceived()) {
+	
+	if (!GPS_obj1->parse(GPS_obj1->lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+		return;  // we can fail to parse a sentence in which case we should just wait for another
+	}
+	Serial.println(GPS_obj1->lastNMEA());
+	Serial.println("GPS Read fix: ");
+	Serial.println(GPS_obj1->fix);
+	Serial.println("GPS Fix Quality: ");
+	Serial.println(GPS_obj1->fixquality);
+}
+
+
---
+++ GPS.h
@@ -0,0 +1,54 @@
+//
+// Created by: Jonathan Midolo
+// Created on: 11/19/2017
+// Last edited by: Jonathan Midolo
+// Last edited bn: 11/20/2017 // bn?
+//
+// Edit notes:
+//		(11-19-2017) Created
+
+#ifndef H2ROVER_GPS_H
+#define H2ROVER_GPS_H
+
+// #include "Arduino.h" // Do we need this...?
+#include <Adafruit_GPS.h>	// need to install "Adafruit GPS Library" library on Arduino IDE
+#include <SoftwareSerial.h>
+#include <PinDeclarations.h>
+
+//This class interfaces the Adafruit Ultimate GPS Breakout v3 with MTK3339 chipset
+class GPS {
+	public:
+    // Constructor and deconstructor require no parameters
+		GPS();
+
+		~GPS();
+    
+    // The GPS module must "warm up" for 30-40s on first boot to get a fix with
+	// dGPS (differential) quality 
+		void initialize(Adafruit_GPS* GPS_obj1, SoftwareSerial* softSerial);
+
+		void gpsRead(Adafruit_GPS* GPS_obj1);
+ 
+
+	private:
+		// Private stuff goes here... I guess.
+		// char c;
+		// uint8_t hour;
+		uint32_t timer = millis();
+		// float lat, lon;
+		// String str;
+		// char timeArray[10], dateArray[10], latArray[10], longArray[10];  
+
+	    int hour;
+		float  lat; // use float or otherwise??? ... or split fractional part separate?
+		float  lon; // use float or otherwise??? ... or split fractional part separate?
+		String str;
+		char timeArray[10];
+		char dateArray[10];
+		char latArray[10];
+		char longArray[10];
+		char c;
+	
+};
+
+#endif //H2ROVER_GPS_H
