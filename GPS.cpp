67149952e6c89225d78b14970a8ffbbab97d6333
jonmidolo <jonathan.midolo@gmail.com>
2017-11-27 18:25:00 -0800

    Removed millis() requirement

--- GPS.cpp
+++ GPS.cpp
@@ -2,10 +2,12 @@
 // Created by: Jonathan Midolo
 // Created on: 11/19/2017
 // Last edited by: Jonathan Midolo
-// Last edited bn: 11/20/2017
+// Last edited on: 11/27/2017
 //
 // Edit notes:
-//		(11-19-2017) Created
+//		(11-19-2017) Created...does not work
+//		(11-22-2017) Works per the work of Daniel Benusovich
+//		(11-27-2017) Cleared issue with millis() within .ino (Removed requirement)
 
 
 #include "GPS.h"
@@ -25,14 +27,15 @@ GPS::~GPS() {
 
 void GPS::initialize() {
 
-	// Initialize GPS
+	// Initialize GPS settings
 	gps_obj.begin(9600);
 	gps_obj.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
 	gps_obj.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
 	gps_obj.sendCommand(PGCMD_ANTENNA);
 	delay(1000);
 
-	while(gps_obj.fix == 0){
+	// Infinite loop until GPS module parses first NMEA sentence (no control over fixquality)
+	while(1){
 		gps_obj.read();
 		if (gps_obj.newNMEAreceived()) {
 			if (!gps_obj.parse(gps_obj.lastNMEA())) return;
@@ -42,6 +45,7 @@ void GPS::initialize() {
 
 int GPS::updateLocation()
 {
+	// Gets new NMEA reading and parses
 	gps_obj.read();
 	if (gps_obj.newNMEAreceived()) {
 		if (!gps_obj.parse(gps_obj.lastNMEA())) return 1;
@@ -49,17 +53,26 @@ int GPS::updateLocation()
 	return 0;
 }
 
-
 int GPS::getData(String* array)
 {
-    array[0] = String(gps_obj.hour);
-    array[1] = String(gps_obj.minute);
-    array[2] = String(gps_obj.seconds);
-    array[3] = String(gps_obj.day);
-    array[4] = String(gps_obj.month);
-    array[5] = String(gps_obj.year);
-    array[6] = String(gps_obj.latitudeDegrees, 8);
-    array[7] = String(gps_obj.longitudeDegrees, 8);
-    return 0;
-}
+	// Infinite loop until data array of strings populates
+	while(1){
+		updateLocation();
 
+		if (timer > millis())  timer = millis();
+
+		if (millis() - timer > 2000) {
+					timer = millis(); // reset the timer
+
+			    array[0] = String(gps_obj.hour);
+			    array[1] = String(gps_obj.minute);
+			    array[2] = String(gps_obj.seconds);
+			    array[3] = String(gps_obj.month);
+			    array[4] = String(gps_obj.day);
+			    array[5] = String(gps_obj.year);
+			    array[6] = String(gps_obj.latitudeDegrees, 10);
+			    array[7] = String(gps_obj.longitudeDegrees, 10);
+			    return 0;
+		}
+	}
+}
--- GPS.h
+++ GPS.h
@@ -2,15 +2,16 @@
 // Created by: Jonathan Midolo
 // Created on: 11/19/2017
 // Last edited by: Jonathan Midolo
-// Last edited bn: 11/20/2017 // bn?
+// Last edited on: 11/27/2017
 //
 // Edit notes:
-//		(11-19-2017) Created
+//		(11-19-2017) Created...does not work
+//		(11-22-2017) Works per the work of Daniel Benusovich
+//		(11-27-2017) Cleared extraneous private data
 
 #ifndef H2ROVER_GPS_H
 #define H2ROVER_GPS_H
 
-// #include "Arduino.h" // Do we need this...?
 #include <Adafruit_GPS.h>	// need to install "Adafruit GPS Library" library on Arduino IDE
 #include <SoftwareSerial.h>
 #include <PinDeclarations.h>
@@ -22,35 +23,18 @@ class GPS {
 		GPS();
 
 		~GPS();
-    
+
     // The GPS module must "warm up" for 30-40s on first boot to get a fix with
-	// dGPS (differential) quality 
+		// dGPS (differential) quality (fixquality = 2)
 		void initialize();
 
 		int updateLocation();
 
 		int getData(String* array);
- 
 
 	private:
-		// Private stuff goes here... I guess.
-		// char c;
-		// uint8_t hour;
 		uint32_t timer = millis();
-		// float lat, lon;
-		// String str;
-		// char timeArray[10], dateArray[10], latArray[10], longArray[10];  
 
-	    int hour;
-		float  lat; // use float or otherwise??? ... or split fractional part separate?
-		float  lon; // use float or otherwise??? ... or split fractional part separate?
-		String str;
-		char timeArray[10];
-		char dateArray[10];
-		char latArray[10];
-		char longArray[10];
-		char c;
-	
 };
 
 #endif //H2ROVER_GPS_H
