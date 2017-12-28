49b8912af58017bbaafae990aec7dfee9b697b1b
eilims <dbenusovich@gmail.com>
2017-12-27 16:42:38 -0800

    correcting errors. Adding additional functionality

--- MoistureSensor.cpp
+++ MoistureSensor.cpp
@@ -4,16 +4,28 @@
 #include "MoistureSensor.h"
 #include "PinDeclarations.h"
 
+MoistureSensor::MoistureSensor() {
+    this->sensorID = 0;
+}
+
 MoistureSensor::MoistureSensor(uint8_t sensorID) {
     this->sensorID = sensorID;
 }
 
 MoistureSensor::~MoistureSensor() {}
 
+void MoistureSensor::initialize(uint8_t sensorID) {
+	this->sensorID = sensorID;
+}
+
 uint16_t MoistureSensor::getData(uint8_t sampleCount) {
     uint32_t dataBank = 0;
     for (int i = 0; i < sampleCount; i++){
         dataBank += analogRead(MOISTURE_INPUT);
+		unsigned long timeStart = millis();
+		while (millis() - timeStart < 100){
+			
+		}
     }
     return dataBank/sampleCount;
 }
@@ -22,9 +34,9 @@ void MoistureSensor::getSensor(sensor_t *sensor) {
     memset(sensor, 0, sizeof(sensor_t));
     strncpy(sensor->name, "", sizeof(sensor->name) - 1);
     sensor->version = 1;
-    sensor->sensor_id =this->sensorId;
+    sensor->sensor_id =this->sensorID;
     sensor->type = SENSOR_TYPE_PROXIMITY;
-    sensor->min_delay = 1000; //1000 microseconds between readings
+    sensor->min_delay = 100; //100 microseconds between readings
     sensor->max_value = 1023; //highest recorded value (very dry
     sensor->resolution = 1; //smallest difference in measurements is 1 unit
 }
--- MoistureSensor.h
+++ MoistureSensor.h
@@ -6,11 +6,14 @@
 #define H2ROVER_MOISTURESENSOR_H
 
 #include <PinDeclarations.h>
+#include <Adafruit_Sensor.h>
 
 class MoistureSensor {
 public:
+	MoistureSensor();
     MoistureSensor(uint8_t sensorID);
     ~MoistureSensor();
+	void initialize(uint8_t sensorID);
     uint16_t getData(uint8_t sampleCount);
     void getSensor(sensor_t* sensor);
 private:
